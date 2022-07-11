2b77252909473ccfc8a8ea3cd5773e5c8a3706a3
KAispread <pain0121@naver.com>
2022-07-11 19:52:15 +0900

    22.07.11

---
+++ CodingTest/src/dataStruct/DivSumRange005.java
@@ -0,0 +1,85 @@
+package dataStruct;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class DivSumRange005 {
+	/* N개의 수 A1, A2, ..., An 이 주어졌을 때 연속된 부분의 합이 M으로 나누어떨어지는 구간의 개수를 구하는
+	 * 프로그램을 작성하시오. 즉, Ai + ... + Aj (i <= j)의 합이 M으로 나누어떨어지는 (i, j)의 쌍의 개수를
+	 * 구하시오.
+	 * 
+	 * 답 접근 => 1. 첫 번째 수부터 J까지의 합이 M으로 나누어떨어지는 경우의 수
+	 *          2. 나머지 값이 같은 합 배열의 개수를 각각 세고, 나머지가 같은 합 배열끼리 선택될 경우의 수 도출 (조합 공식 이용 nCr) 
+	 * 
+	 * */
+	public static void main(String[] args) {
+		DivSumRange005 sample = new DivSumRange005();
+		try {
+			sample.myAnswer();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void myAnswer() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(reader.readLine());
+		
+		int N = Integer.parseInt(token.nextToken());
+		int M = Integer.parseInt(token.nextToken());
+		
+		int[] A = new int[N + 1];
+		int[] S = new int[N + 1];
+		token = new StringTokenizer(reader.readLine());
+		for (int i = 1; i < N + 1; i++) {
+			A[i] = Integer.parseInt(token.nextToken());
+			S[i] = S[i - 1] + A[i];
+		}
+		
+		int count = 0;
+		int[] C = new int[M];
+		for (int i = 1; i < N + 1; i++) {
+			S[i] = S[i] % M;
+			if (S[i] == 0) count++;
+			C[S[i]]++;
+		}
+		
+		for (int i = 0; i < M; i++) {
+			count += (C[i] * (C[i] - 1)) / 2;
+		}
+		System.out.println(count);
+	}
+	
+	public void correctAnswer() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		long[] S = new long[N];
+		long[] C = new long[M];
+		long answer = 0;
+		S[0] = sc.nextInt();
+		
+		for(int i = 1; i < N; i++) {
+			S[i] = S[i - 1] + sc.nextInt();
+		}
+		
+		for (int i =0; i < N; i++) {
+			// 합 배열의 모든 값에 나머지 연산 수행
+			int remainer = (int) (S[i] % M);
+			
+			// 0 ~ i 까지의 구간 합 자체가 0일때 정답에 더하기
+			if (remainer == 0) answer++;
+			
+			// 나머지가 같은 인덱스의 개수 카운트하기
+			C[remainer]++;
+		}
+		
+		for (int i = 0; i < M; i++) {
+			if (C[i] > 1) answer += (C[i] * (C[i] - 1)) / 2;
+		}
+		System.out.println(answer);
+	}
+}
---
+++ CodingTest/src/dataStruct/SumConnectedNumber006.java
@@ -0,0 +1,66 @@
+package dataStruct;
+
+import java.util.Scanner;
+public class SumConnectedNumber006 {
+	/*	연속된 자연수의 합 구하기
+	 *  자연수 N은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다. (1 <= N <= 10000000) 의 자연수 N에서
+	 *  몇 개의 연속된 자연수의 합으로 나타낼 수 있는지 확인하고 싶다. ex) 15 = 15, 7+8, 4+5+6, 1+2+3+4+5
+	 *  
+	 *  풀이 => 투 포인터를 사용.
+	 *        start_index와 end_index를 sum과 N을 비교하며 키움. 이후 sum값을 계산 (매번 아예 새로 계산하는거 절대 x)
+	 * */
+	
+	public static void main(String[] args) {
+		SumConnectedNumber006 sample = new SumConnectedNumber006();
+		sample.correctAnswer();
+	}
+	
+	public void myAnswer() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 1; // 자기 자신을 고르는 경우 미리 포함
+		int start_index = 1;
+		int end_index = 1;
+		int sum = 1;
+		
+		while(end_index < N - 1) {
+			if(sum > N) {
+				sum -= start_index;
+				start_index++;
+			} else if (sum < N) {
+				end_index++;
+				sum += end_index;
+			} else if (sum == N) {
+				count++;
+				end_index++;
+				sum += end_index;
+			}
+		}
+		System.out.println(count);
+	}
+	
+	public void correctAnswer() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 1; // 자기 자신을 고르는 경우 미리 포함
+		int start_index = 1;
+		int end_index = 1;
+		int sum = 1;
+		
+
+		while(end_index != N) {
+			if (sum == N) {
+				count++;
+				end_index++;
+				sum += end_index;
+			} else if (sum > N) {
+				sum -= start_index;
+				start_index++;
+			} else {
+				end_index++;
+				sum += end_index;
+			}
+		}
+		System.out.println(count);
+	}
+}
